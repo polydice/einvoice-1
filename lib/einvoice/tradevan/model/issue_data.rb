add208fff7e008211c51f8fb1990de8189e97bd6
wells <deltaguita@gmail.com>
2025-05-22 18:04:32 +0800

    receiverAddrZip max size 5 -> 6

--- lib/einvoice/tradevan/model/issue_data.rb
+++ lib/einvoice/tradevan/model/issue_data.rb
@@ -103,8 +103,9 @@ module Einvoice
 
         # Type BA04 I R G BB04 H A
         validates :receiverName, allow_blank: true, length: { maximum: 30 }, if: proc { %w(BA04 I R G BB04 H A).include?(self.type) }
-        validates :receiverAddrZip, allow_blank: true, length: { maximum: 5 }, if: proc { %w(BA04 I R G BB04 H A).include?(self.type) }
+        validates :receiverAddrZip, allow_blank: true, length: { maximum: 6 }, if: proc { %w(BA04 I R G BB04 H A).include?(self.type) }
         validates :receiverAddrRoad, allow_blank: true, length: { maximum: 100 }, if: proc { %w(BA04 I R G BB04 H A).include?(self.type) }
+        validates :receiverEmail, allow_blank: true, length: { maximum: 400 }, if: proc { %w(BA04 I G R BB04 H A).include?(self.type) }
         validates :receiverMobile, allow_blank: true, length: { maximum: 15 }, if: proc { %w(BA04 I R G BB04 H A).include?(self.type) }
 
         # Type BA04 I G
